9db07c5d802c6c017007d8de0870c92275db9c22
TK Nguyễn <khanh.npt2127@hotmail.com.vn>
2019-04-21 23:46:37 +0200

    ADD: X when clicked

--- src/index.js
+++ src/index.js
@@ -6,10 +6,20 @@ import * as serviceWorker from './serviceWorker';
 
 
 class Square extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+    }
+
     render() {
       return (
-        <button className="square">
-            {this.props.value}
+        <button className="square" 
+                onClick={() => this.setState({value: 'X'})}
+        >
+            {this.state.value}
         </button>
       );
     }
